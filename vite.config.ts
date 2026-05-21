23606eab1cb5a8fefd66ad48a7da1371cb767fac
Codingwithriha <rihashehzadi2003@gmail.com>
2026-05-21 13:16:32 +0500

    fix: add path aliasing and build configuration to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
